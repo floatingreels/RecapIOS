66a9b01938d7eecd0bee69c4e74e99769f6b41de
David Gunzburg <david.gunzburg@gmal.com>
2020-01-24 09:55:25 +0100

    Initial Commit

---
+++ RecapIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecapIOS
+//
+//  Created by mobapp02 on 24/01/2020.
+//  Copyright © 2020 mobapp02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
